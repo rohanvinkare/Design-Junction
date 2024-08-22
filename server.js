46da53d8b4caca611d8efd114a55b1b2d6372168
Rohan Vinkare <rohanvinkare2022@gmail.com>
2024-08-22 13:45:22 +0530

    first commit

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.set('public', path.join(__dirname, 'public'));
+
+
 
 
 app.use(cookieParser());
